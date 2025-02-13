a6935d9b3b437fa41b0aa07205c47d35a5ffb0e6
Anastasiia Dotsina <tigralbus@yandex.ru>
2025-02-13 12:52:05 +0300

    fix comment: Нужно исправить: нужно конкатенировать урлы, чтобы потом можно было быстро запускать тесты на разных окружениях. Пример:
    URL = 'https://stellarburgers.nomoreparties.site/'
    PERSONAL_ACCOUNT_URL = f'{URL}account/profile'

--- constants.py
+++ constants.py
@@ -1,13 +1,13 @@
 class Constants:
-    URL = 'https://stellarburgers.nomoreparties.site/'
-    PERSONAL_ACCOUNT_URL = 'https://stellarburgers.nomoreparties.site/account/profile'
-    FORGOT_PASSWORD_URL = 'https://stellarburgers.nomoreparties.site/forgot-password'
-    RESTORE_PASSWORD_URL = 'https://stellarburgers.nomoreparties.site/reset-password'
-    ORDERS_HISTORY_URL = 'https://stellarburgers.nomoreparties.site/account/order-history'
-    LOGIN_URL = 'https://stellarburgers.nomoreparties.site/login'
-    INGREDIENT_BUN_DETAILS_URL = 'https://stellarburgers.nomoreparties.site/ingredient/61c0c5a71d1f82001bdaaa6d'
-    ORDERS_LIST_URL = 'https://stellarburgers.nomoreparties.site/feed'
-    API_URL = 'https://stellarburgers.nomoreparties.site/api'
+    URL = 'https://stellarburgers.nomoreparties.site'
+    PERSONAL_ACCOUNT_URL = f'{URL}/account/profile'
+    FORGOT_PASSWORD_URL = f'{URL}/forgot-password'
+    RESTORE_PASSWORD_URL = f'{URL}/reset-password'
+    ORDERS_HISTORY_URL = f'{URL}/account/order-history'
+    LOGIN_URL = f'{URL}/login'
+    INGREDIENT_BUN_DETAILS_URL = f'{URL}/ingredient/61c0c5a71d1f82001bdaaa6d'
+    ORDERS_LIST_URL = f'{URL}/feed'
+    API_URL = f'{URL}/api'
 
 class UserData:
     RESTORE_EMAIL = 'test_dkbahbsfljh@ya_test.ru'
